f7f5b06dc04b06e63cb5e2f8bda2acb62cc51fe0
Mathieu Chan <mathieu1.chan@gmail.com>
2025-11-28 16:38:18 +0100

    main-activity: refactor scaffold and pick media part

--- app/src/main/java/fr/pass/local_password/MainActivity.kt
+++ app/src/main/java/fr/pass/local_password/MainActivity.kt
@@ -1,5 +1,7 @@
 package fr.pass.local_password
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -10,9 +12,10 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +23,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,46 +51,56 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun decodeQRCode(context: Context, uri: Uri?, onSuccess: (Barcode?) -> Unit) {
+    // Callback is invoked after the user selects a media item or closes the
+    // photo picker.
+    if (uri == null) {
+        Log.d("PhotoPicker", "No media selected")
+        return
+    }
+
+    Log.d("PhotoPicker", "Selected URI: $uri")
+    val options = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+        .build()
+    val barcodeScanner = BarcodeScanning.getClient(options)
+    val image = InputImage.fromFilePath(context, uri)
+    val task = barcodeScanner.process(image)
+    task.addOnSuccessListener { barcodes ->
+        if (barcodes.isEmpty()) {
+            Log.v(MANUAL_TESTING_LOG, "No barcode has been detected")
+        } else {
+            // Assume there is only one QR code in the image
+            onSuccess(barcodes[0])
+        }
+    }
+}
+
 @Composable
 fun SelectImageFromPicker() {
     val context = LocalContext.current
     var barcode by remember { mutableStateOf<Barcode?>(null) }
 
     // Registers a photo picker activity launcher in single-select mode.
-    val pickMedia =
-        rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-            // Callback is invoked after the user selects a media item or closes the
-            // photo picker.
-            if (uri == null) {
-                Log.d("PhotoPicker", "No media selected")
-                return@rememberLauncherForActivityResult
-            }
-
-            Log.d("PhotoPicker", "Selected URI: $uri")
-            val options = BarcodeScannerOptions.Builder()
-                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-                .build()
-            val barcodeScanner = BarcodeScanning.getClient(options)
-            val image = InputImage.fromFilePath(context, uri)
-            val task = barcodeScanner.process(image)
-            task.addOnSuccessListener { barcodes ->
-                if (barcodes.isEmpty()) {
-                    Log.v(MANUAL_TESTING_LOG, "No barcode has been detected")
-                } else {
-                    // Assume there is only one QR code in the image
-                    barcode = barcodes[0]
-                }
-            }
-        }
+    val pickMedia = rememberLauncherForActivityResult(
+        ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri -> decodeQRCode(context, uri) { result -> barcode = result } }
+    )
     Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                // Launch the photo picker and let the user choose only images.
-                pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                        onClick = {
+                            // Launch the photo picker and let the user choose only images.
+                            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                        }) {
+                        Icon(Icons.Filled.Photo, contentDescription = "Select photo")
+                    }
+                },
+            )
+        },
     ) { innerPadding ->
         val code = barcode
         if (code != null && code.rawValue != null) {
